b8e938fd3a16c93bd9813d3643fcabc65779dc34
krid551997033 <chaiyapong.sainin@gmail.com>
2016-11-22 13:07:59 +0700

    add new file Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello travis");
 		System.out.println("My name is Chaiyapong Saiinin");
+		System.out.println("My nickname is KRID");
 		}
 }
